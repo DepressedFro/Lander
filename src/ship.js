c5be85e00c909f2832ecb6bef5f812894ed4dad1
Cortez Gray <cgray6@win.cis.ksu.edu>
2017-10-11 11:23:17 -0500

    daily class changes

---
+++ src/ship.js
@@ -0,0 +1,23 @@
+export default class Ship
+{
+  constructor(screenWidth, screenHeight)
+  {
+    this.x = screenwidth / 2;
+    this.y = 10;
+    this.angle = 0;
+  }
+
+  render(ctx)
+  {
+    ctx.save();
+    ctx.strokeStyle = "#fff";
+    ctx.beginPath();
+    ctx.rotate(this.angle);
+    ctx.moveTo(this.x, this.y + 5);
+    ctx.lineTo(this.x + 2, this.y - 5);
+    ctx.lineTo(this.x -= 2, this.y - 5);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.restore();
+  }
+}
